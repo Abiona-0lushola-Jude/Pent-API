197647065ed3cc70d061a26f3fadd995a4556d06
Abiona Olushola Jude <abionaolushola11@yahoo.com>
2022-08-22 09:18:10 +0100

    done

--- Routers/router.js
+++ Routers/router.js
@@ -30,6 +30,16 @@ router.post('/post', async (req,res)=>{
     }
 })
 
+// deleting one User
+router.delete('/user/delete/:id', async(req,res)=>{
+    try {
+        const userDeleted = await user.findByIdAndDelete({_id:req.params.id}) 
+        res.json({message: `user with id ${req.params.id} deleted`})
+    } catch (err) {
+        res.json({message: err.message})
+    }
+})
+
 
 // posting reviews 
 router.post('/post/review',async (req,res)=>{
@@ -53,6 +63,16 @@ router.get('/review', async (req,res)=>{
     try {
         const allReview = await review.find().sort({uploadTime:'desc'})
         res.json(allReview)
+    } catch (err) {
+        res.json({message: err.message})
+    }
+})
+
+// deleting oneReview
+router.delete('/review/delete/:id', async(req,res)=>{
+    try {
+        const reviewDeleted = await review.findByIdAndDelete({_id:req.params.id}) 
+        res.json(reviewDeleted, {message: `review made id ${req.params.id} has been deleted`})
     } catch (err) {
         res.json({message: err.message})
     }
